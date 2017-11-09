4b2d07edb318150fe4eb4cfea3a25909c6467ea1
Nicolas Martignole <nicolas@martignole.net>
2017-11-09 17:10:02 +0100

    Webuser listAll as JSON

--- app/controllers/WebuserController.scala
+++ app/controllers/WebuserController.scala
@@ -4,11 +4,12 @@ package controllers
 import javax.inject._
 
 import models.Webuser
+import play.api.libs.json.Json
 import play.api.mvc._
 import services.WebuserService
 
 @Singleton
-class WebuserController @Inject()(messagesAction: MessagesActionBuilder, cc: ControllerComponents, webuserService:WebuserService) extends AbstractController(cc) {
+class WebuserController @Inject()(messagesAction: MessagesActionBuilder, cc: ControllerComponents, webuserService: WebuserService) extends AbstractController(cc) {
 
   def newWebuser = messagesAction {
     implicit request: MessagesRequest[AnyContent] =>
@@ -22,13 +23,29 @@ class WebuserController @Inject()(messagesAction: MessagesActionBuilder, cc: Con
         hasErrors => BadRequest(views.html.newWebuser(hasErrors)),
         validNewWebuser => {
           webuserService.saveWebuser(validNewWebuser)
-          Redirect(routes.WebuserController.listAllWebusers()).flashing("success"->"Webuser created")
+          Redirect(routes.WebuserController.listAllWebusers()).flashing("success" -> "Webuser created")
         }
       )
   }
 
-  def listAllWebusers = messagesAction{
-    implicit req:MessagesRequest[AnyContent]=>
-      Ok(views.html.listAllWebusers(webuserService.listAll))
+  def listAllWebusers = messagesAction {
+    implicit req: MessagesRequest[AnyContent] =>
+
+      if (req.headers("Accept").startsWith("text/html")) {
+        Ok(views.html.listAllWebusers(webuserService.listAll))
+      } else {
+
+        // Permet de transformer un Webuser en JSON
+        import Webuser.webuserFormat
+
+        // Recupere la liste de tous les webusers
+        val webusers = Json.toJson(webuserService.listAll)
+        // Construction de notre réponse
+        val jsonResult = Json.toJson(Map("users" -> webusers))
+
+        // Yo lo c'est envoyé
+        Ok(jsonResult).as(JSON)
+      }
+
   }
 }
